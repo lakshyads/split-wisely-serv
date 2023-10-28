8eee5ca5da79443131657b7eba2468fa8acd3943
Lakshya Dev Singh <lakshyadev@live.com>
2023-10-28 16:18:44 +0530

    init new SprintBoot app for SplitWiselyServ

---
+++ src/main/java/com/lakshya/splitwiselyserv/SplitWiselyServApplication.java
@@ -0,0 +1,13 @@
+package com.lakshya.splitwiselyserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitWiselyServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitWiselyServApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lakshya/splitwiselyserv/SplitWiselyServApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakshya.splitwiselyserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitWiselyServApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
